ae3459805263f643471e36f2561cc2edf747ef65
MrPickle311 <macmacroll@gmail.com>
2022-11-01 10:37:49 +0100

    Updated robot files

---
+++ src/mobile_robot/scripts/gps_to_xy.py
@@ -0,0 +1,27 @@
+import rospy
+import math
+from sensor_msgs.msg import *
+from nav_msgs.msg import *
+from geometry_msgs.msg import *
+
+EARTH_RADIUS = 6378.137  # kilometers
+
+xy_pub = rospy.Publisher('/gps/xy', Vector3, queue_size=1000)
+
+
+def convert_gps_to_xy(gps_data: NavSatFix) -> float:
+    global EARTH_RADIUS, xy_pub
+    φ0 = 0  # where φ0 denotes a latitude close to the center of your map.
+    vec = Vector3()
+    vec.x = EARTH_RADIUS*math.radians(gps_data.latitude)*1000
+    vec.y = EARTH_RADIUS*math.radians(gps_data.longitude)*math.cos(φ0)*1000
+    vec.z = 0
+    vec.y *= -1  # it must be almost equal with odom, axes direction must agree with odometry
+    xy_pub.publish(vec)
+
+
+rospy.init_node('gps_to_xy_converter')
+rospy.Subscriber('/gps/fix', NavSatFix,
+                 convert_gps_to_xy)
+
+rospy.spin()
